91959745f73c04adbfd411a8ce5be815b4993766
parkah@mit.edu <parkah@mit.edu>
2015-08-07 17:45:38 -0700

    UPDATE: through chapter 4 (added forms) to page, now working on db

--- hello.py
+++ hello.py
@@ -1,25 +1,36 @@
-from flask import Flask, render_template
-from flask import redirect
-
-
-app = Flask(__name__)
-
-#importing Flask-Script
+from flask import Flask, render_template,session, redirect, url_for, flash
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'peter gregory'
 
 #initializing the imported modules
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data
+		form.name.data = ''
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'))
 
 
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html', name=name)
 
 @app.route('/user/<id>')
 def get_user(id):
