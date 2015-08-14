faf1d3d9e5dc2f5d7c9c6993593f529fc9b37635
parkah@mit.edu <parkah@mit.edu>
2015-08-13 19:33:36 -0700

    UPDATE: app now works with emails, up to chapter 6a

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 from flask import Flask, render_template,session, redirect, url_for, flash
 from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
@@ -8,7 +9,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
-
+from flask.ext.mail import Mail, Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,16 +18,31 @@ app.config['SECRET_KEY'] = 'something secret'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
 	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
-db = SQLAlchemy(app)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True 
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'petergriggs17@gmail.com'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 
 #initializing the imported modules
+db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
+
+def send_email(to, subject, template, **kwargs):
+	msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject, 
+			sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+	msg.body = render_template(template + '.txt', **kwargs)
+	msg.html = render_template(template + '.html', **kwargs)
+	mail.send(msg)
 
 #defines the roles table using SQLAlchemy ORM
 class Role(db.Model):
@@ -71,11 +87,12 @@ def index():
 			user = User(username = form.name.data)
 			db.session.add(user)
 			session['known'] = False
+			if app.config['FLASKY_ADMIN']:
+				send_email(app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=user)
 		else:
 			session['known'] = True
 
 		session['name'] = form.name.data
-		form.name.data = ''
 		return redirect(url_for('index'))
 	return render_template('index.html', 
 		form=form, name=session.get('name'),
